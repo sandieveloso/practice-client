9dc908f6dc673b16c067d8e43ea2714e82d839b8
Sandie Veloso <sveloso@loc8.com>
2016-08-11 17:31:21 +1000

    Configuring Angular routes and Bootstrap Sass structure

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,10 +4,7 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
+
 
 angular.module('priume', ['ui.router', 'ui.bootstrap', 'toastr'])
   .constant('malarkey', malarkey)
@@ -15,8 +12,4 @@ angular.module('priume', ['ui.router', 'ui.bootstrap', 'toastr'])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
-  .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,12 +1,16 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
-  'ngInject';
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
-    });
+	'ngInject';
+	$stateProvider
+		.state('home', {
+			url: "/",
+			templateUrl: "app/main/main.html",
+			controller: "MainController",
+			controllerAs: "main"
+	})
+		.state('brands', {
+			url: "/brands",
+			templateUrl: "app/brands/brands.html"
+			});
 
-  $urlRouterProvider.otherwise('/');
+	$urlRouterProvider.otherwise('/');
 }
